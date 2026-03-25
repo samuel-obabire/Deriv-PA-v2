39b4d6103257d1a2616543990ddade11e55725e2
Smbirey <smbirey@gmail.com>
2026-03-25 15:32:40 +0100

    setup next-themes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Space_Grotesk } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const space = Space_Grotesk({
 	subsets: ["latin"],
@@ -19,8 +20,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={space.className}>
-			<body>{children}</body>
+		<html lang="en" className={space.className} suppressHydrationWarning>
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
