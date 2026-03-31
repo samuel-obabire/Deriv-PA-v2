84e05a4c52c74abf37ff57f5926d27a275c7ec6f
Smbirey <smbirey@gmail.com>
2026-03-31 20:50:19 +0100

    fix build issue

--- apps/web/lib/validations/env/server.ts
+++ apps/web/lib/validations/env/server.ts
@@ -4,7 +4,9 @@ const ServerSchema = z.object({
 	DATABASE_URL: z.url(),
 	BASE_URL: z.url(),
 	BETTER_AUTH_SECRET: z.string(),
-	NODE_ENV: z.enum(["production", "development", "test"]),
+	NODE_ENV: z
+		.enum(["production", "development", "test"])
+		.default("development"),
 });
 
 export const serverEnv = ServerSchema.parse(process.env);
