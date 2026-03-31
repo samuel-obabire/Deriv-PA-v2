389ed97aebc96b596a99e08b4b33bf582b056dba
Smbirey <smbirey@gmail.com>
2026-03-31 20:04:17 +0100

    fix build issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ["@repo/db"],
+	transpilePackages: ["@repo/db", "@repo/utils"],
 	serverExternalPackages: ["pino, pino-pretty"],
 };
 
