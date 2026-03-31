7b11eaaf4edd30d3cf3340111dc1398c47546869
Smbirey <smbirey@gmail.com>
2026-03-31 22:13:17 +0100

    fix deploy issue

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 5050);
+
+	const PORT = process.env.PORT || 5050;
+
+	await app.listen(PORT, "0.0.0.0");
 }
 bootstrap();
