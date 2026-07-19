6323fa32b98f1ed9f88ab5081cd067b3224da412
Smbirey <smbirey@gmail.com>
2026-07-19 17:43:03 +0100

    bug fix

--- apps/client-kyc/app/kyc/deriv/callback/page.tsx
+++ apps/client-kyc/app/kyc/deriv/callback/page.tsx
@@ -5,7 +5,7 @@ import ROUTES from "@/lib/constants/routes";
 import { verifySession } from "@/lib/session";
 
 const DerivCallbackPage = async () => {
-	await verifySession(ROUTES.KYC_DERIV_CALLBACK);
+	await verifySession(ROUTES.DERIV_CALLBACK);
 
 	return (
 		<Suspense
